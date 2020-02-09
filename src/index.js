bc52901c229c0d22e902a6aa9e1afbf5526f3a69
ceemion <fiyinsimeon@gmail.com>
2020-02-09 19:49:19 +0100

    feat: Setup middleware with thunk and logger

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+import middleware from './middleware';
 
-const store = createStore(reducers)
+const store = createStore(reducers, middleware)
 
 ReactDOM.render(
   <Provider store={store}>
