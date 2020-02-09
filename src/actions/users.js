973b1cbaaaaa7db59fa4cf656c6ca8cec6177b6b
ceemion <fiyinsimeon@gmail.com>
2020-02-09 18:53:20 +0100

    feat: Setup user action to receive users and dispatch to store

---
+++ src/actions/users.js
@@ -0,0 +1,18 @@
+import { getUsers } from "../utils/api";
+import { RECEIVE_USERS } from "./constants";
+
+function receiveUsers (users) {
+  return {
+    type: RECEIVE_USERS,
+    users,
+  }
+}
+
+export function handleGetUsers () {
+  return (dispatch) => {
+    return getUsers()
+      .then(({ users }) => {
+        dispatch(receiveUsers(users))
+      })
+  }
+}
