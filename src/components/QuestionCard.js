c499f89a75b14ae08198ec85daf1010431e30856
ceemion <fiyinsimeon@gmail.com>
2020-02-15 20:01:30 +0100

    style: Question card design

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -3,37 +3,39 @@ import { connect } from 'react-redux';
 import helpers from '../utils/helpers';
 import { Link } from 'react-router-dom';
 
-class QuestionCard extends Component {
-  render() {
-    const { question } = this.props;
-
-    if (question === null) {
-      return (
-        <div>This question does not exist.</div>
-      )
-    }
-
+const QuestionCard = ({ question }) => {
+  if (question === null) {
     return (
-      <div style={{border: '1px solid orange', margin: '10px auto'}}>
-        <div>
+      <div>This question does not exist.</div>
+    )
+  }
+
+  return (
+    <div className="question-card">
+      <div className="user">
+        <div className="image">
           <img src={question.avatar} alt={`${question.avatar} avatar`} width={100} />
         </div>
-        <div>
-          <p>{ question.name }</p>
-        </div>
 
         <div>
-          <p>Would you rather...</p>
-          <p>{question.optionOneText}</p>
-          <p>{question.optionTwoText}</p>
+          <h4 className="name">
+            { question.name }
+          </h4>
         </div>
+      </div>
 
-        <Link to={`questions/${question.id}`}>
+      <div className="summary">
+        <h4>Would you rather...</h4>
+        <p>{question.optionOneText}</p>
+        <p>or</p>
+        <p>...</p>
+
+        <Link to={`questions/${question.id}`} className="link">
           View Poll
         </Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = ({ questions, users }, { id }) => {
