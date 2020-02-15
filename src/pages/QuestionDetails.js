2c9c0395e5407e53714adf9411782092b70fbdd5
ceemion <fiyinsimeon@gmail.com>
2020-02-15 21:56:37 +0100

    style: Question details page, vote"

--- src/pages/QuestionDetails.js
+++ src/pages/QuestionDetails.js
@@ -4,6 +4,7 @@ import helpers from '../utils/helpers';
 import { withRouter } from 'react-router-dom';
 import { handleSaveQuestionVote } from '../actions/questions';
 import NavBar from '../components/NavBar';
+import Button from '../components/Button';
 
 const QuestionDetails = (props) => {
   const {
@@ -11,7 +12,8 @@ const QuestionDetails = (props) => {
     question,
     answered,
     dispatch,
-    totalUsers
+    totalUsers,
+    loadingBar,
   } = props;
 
   const [vote, setVote] = useState('');
@@ -22,50 +24,82 @@ const QuestionDetails = (props) => {
     setVote('')
   }
 
+  const setActiveClass = (option) => {
+    return vote === option ? 'option active' : 'option'
+  }
+
+  const isBtnDisabled = () => {
+    return !vote || loadingBar.default === 1
+  }
+
   return (
-    <div>
-      <NavBar />
+    <>
+    <NavBar />
 
-      <h4>{question.name} { answered ? 'asked:' : 'asks...' }</h4>
+    <div className="wrapper question-details">
+      <h5>{question.name} { answered ? 'asked:' : 'asks...' }</h5>
 
-      <img src={question.avatar} alt={`${question.avatar} avatar`} width={100} />
-
-      { answered ? (
-        <div>
-          <p>Poll Results</p>
-          <p>
-            ({ question.optionOneVotes.includes(authUser) && <span>You answered this.</span> })
-            {question.optionOneText}, {question.optionOneVotes.length} of {totalUsers}, {helpers.calcPercentage(question.optionOneVotes.length, totalUsers)}%
-          </p>
-          
-          <p>
-            ({ question.optionTwoVotes.includes(authUser) && <span>You answered this.</span> })
-            {question.optionTwoText}, {question.optionTwoVotes.length} of {totalUsers}, {helpers.calcPercentage(question.optionTwoVotes.length, totalUsers)}%
-          </p>
+      <div className="body">
+        <div className="user">
+          <img src={question.avatar} alt={`${question.avatar} avatar`} width={100} />
         </div>
-      ) : (
-        <div>
-          <p>Would you rather?</p>
-          <p onClick={() => setVote('optionOne')}>{question.optionOneText}</p>
-          <p onClick={() => setVote('optionTwo')}>{question.optionTwoText}</p>
-        </div>
-      ) }
 
-      { answered || (
-        <div>
-          <button onClick={handleSubmitVote}>Vote</button>
+        <div className="questions">
+          { answered ? (
+            <>
+              <h3>Poll Results</h3>
+
+              <div className={question.optionOneVotes.includes(authUser) ? 'answer' : ''}>
+                ({ question.optionOneVotes.includes(authUser) && <span>You answered this.</span> })
+                {question.optionOneText}, {question.optionOneVotes.length} of {totalUsers}, {helpers.calcPercentage(question.optionOneVotes.length, totalUsers)}%
+              </div>
+
+              <div className={question.optionTwoVotes.includes(authUser) ? 'answer' : ''}>
+                ({ question.optionTwoVotes.includes(authUser) && <span>You answered this.</span> })
+                {question.optionTwoText}, {question.optionTwoVotes.length} of {totalUsers}, {helpers.calcPercentage(question.optionTwoVotes.length, totalUsers)}%
+              </div>
+            </>
+          ) : (
+            <>
+              <h3>Would you rather?</h3>
+
+              <div className="options">
+                <div className={setActiveClass('optionOne')} onClick={() => setVote('optionOne')}>
+                  {question.optionOneText}
+                </div>
+
+                <div className={setActiveClass('optionTwo')} onClick={() => setVote('optionTwo')}>
+                  {question.optionTwoText}
+                </div>
+              </div>
+            </>
+          ) }
+
+          { answered || (
+            <div className="cta">
+              <Button
+                id="vote-btn"
+                text="Vote Now"
+                type="primary"
+                onClick={handleSubmitVote}
+                isDisabled={isBtnDisabled()}
+              />
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
+    </>
   )
 }
 
-const mapStateToProps = ({ authUser, questions, users }, { match }) => {
+const mapStateToProps = ({ authUser, questions, users, loadingBar }, { match }) => {
   const { question_id } = match.params;
   const question = questions[question_id]
 
   return {
     authUser,
+    loadingBar,
     question: helpers.formatQuestion(question, users),
     answered: helpers.questionAnswered(question, authUser),
     totalUsers: Object.keys(users).length,
