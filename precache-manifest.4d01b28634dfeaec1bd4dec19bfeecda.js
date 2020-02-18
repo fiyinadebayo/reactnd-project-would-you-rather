0214bd61b36d38f5df6dcb8ccf71951ddc28865f
ceemion <fiyinsimeon@gmail.com>
2020-02-18 13:08:06 +0100

    Updates

---
+++ precache-manifest.4d01b28634dfeaec1bd4dec19bfeecda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47691d552dc36d1cfe24a0bb4999a624",
+    "url": "/reactnd-project-would-you-rather/index.html"
+  },
+  {
+    "revision": "21b3260b59f9c1a4447a",
+    "url": "/reactnd-project-would-you-rather/static/css/main.41d57260.chunk.css"
+  },
+  {
+    "revision": "7f5ab3f241aab295a060",
+    "url": "/reactnd-project-would-you-rather/static/js/2.ee3d0bab.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactnd-project-would-you-rather/static/js/2.ee3d0bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b3260b59f9c1a4447a",
+    "url": "/reactnd-project-would-you-rather/static/js/main.590f59bf.chunk.js"
+  },
+  {
+    "revision": "936ae3e36dfe75164824",
+    "url": "/reactnd-project-would-you-rather/static/js/runtime-main.f76f4f93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactnd-project-would-you-rather/static/media/react-logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ae1af16ffc2f55bd739da81176302e9",
+    "url": "/reactnd-project-would-you-rather/static/media/redux-logo.5ae1af16.svg"
+  }
+]);
